1dd9d1e6ffd5a23943ec3de62de5db3e478a78e4
Saksham Gupta <sakshamgupta.ai@gmail.com>
2026-03-11 21:03:58 +0530

    abstraction and Coderunner added

---
+++ src/objectOrientedProgramming/Coderunner.java
@@ -0,0 +1,7 @@
+package objectOrientedProgramming;
+
+public class Coderunner {
+    public static void main(String[] args) {
+        System.out.println("Doing OOPs in Java");
+    }
+}
---
+++ src/objectOrientedProgramming/pillars/Abstraction.java
@@ -0,0 +1,7 @@
+package objectOrientedProgramming.pillars;
+
+public class Abstraction {
+    public static void main(String[] args) {
+        System.out.println("Abstraction");
+    }
+}
